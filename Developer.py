4601164b77da4cf27d644808376b2a88dbf3a212
hojimurod07 <hojimurad07@gmail.com>
2024-04-03 18:24:54 +0500

    ikkita funkiya developerga qoshildi

--- Developer.py
+++ Developer.py
@@ -10,3 +10,9 @@ class Developer(Human):
     def __init__(self,name,age,yonalish):
         super().__init__(name,age)
         self.yonalish = yonalish
+
+    def codeYozish(self):
+        print("Dasturchi code yozmoqda")
+
+    def cofee_ichish(self):
+        print("Cofe ishich vaqti")
